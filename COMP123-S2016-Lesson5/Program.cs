d74cc6774e391d900d072d91ce2045d420931721
Janelle Nomad <jheron3@my.centennialcollege.ca>
2016-06-10 19:06:10 -0400

    Version 1.1 - Fixed spelling mistakes

--- COMP123-S2016-Lesson5/Program.cs
+++ COMP123-S2016-Lesson5/Program.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
  * <summary>
  * This is program simulates shuffling a deck
  * </summary>
- * Version 1.0 
+ * Version 1.1: Fixed spelling mistakes  
  */
 namespace COMP123_S2016_Lesson5
 {
@@ -35,12 +35,11 @@ namespace COMP123_S2016_Lesson5
             deck.Display(); // display the initial state of the deck
 
             deck.Shuffle(); // shuffle the deck
+            /* The shuffle method, takes the card that's picked and removes it from the deck*/
+
             deck.Display(); // display the deck after shuffling
 
-
         } // end Main
-
-        
 
     } // end Program
 
