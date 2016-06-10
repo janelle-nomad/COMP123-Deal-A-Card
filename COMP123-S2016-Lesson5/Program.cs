b02dab31c73e30bd3b3e909bd1f1e154a7b45987
Janelle Nomad <jheron3@my.centennialcollege.ca>
2016-06-10 18:59:42 -0400

    Add project files.

---
+++ COMP123-S2016-Lesson5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Janelle Heron
+ * 300839820    
+ * "Deal a Card" Assignment 
+ * <summary>
+ * This is program simulates shuffling a deck
+ * </summary>
+ * Version 1.0 
+ */
+namespace COMP123_S2016_Lesson5
+{
+    /**
+     * This class is the "driver" class for our Program
+     * 
+     * @class Program
+     */
+    public class Program
+    {
+        /**
+         * the main method for our driver class Program
+         * 
+         * @method Main
+         * @param {sting[]} args
+         * @returns {void}
+         */
+        public static void Main(string[] args)
+        {
+            Deck deck = new Deck(); // create a deck of cards
+            deck.Display(); // display the initial state of the deck
+
+            deck.Shuffle(); // shuffle the deck
+            deck.Display(); // display the deck after shuffling
+
+
+        } // end Main
+
+        
+
+    } // end Program
+
+} // end namespace
